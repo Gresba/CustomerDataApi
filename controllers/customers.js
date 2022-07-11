48836515bbf3db771e047428c27c0164081553b8
gresba <kimpaul0709@gmail.com>
2022-07-11 18:16:58 -0400

    Created controller for customer

--- controllers/customers.js
+++ controllers/customers.js
@@ -24,3 +24,41 @@ export const getCustomer = (req, res) => {
             res.json({message: error.message})
         })
 }
+
+export const createCustomer = (req, res) => {
+    Customer.create(req.body)
+        .then((customer) => {
+            res.status(200).json(customer)
+            console.log("Successfully Created a customer")
+        })
+        .catch(error => {
+            console.error(error)
+            res.json({message: error.message})
+        })
+}
+
+export const updateCustomer = (req, res) => {
+    const id = req.params.id;
+    Customer.findByIdAndUpdate(id, req.body)
+        .then(() => {
+            console.log("Successfully updated card")
+            res.status(200).json(req.body)
+        })
+}
+
+export const deleteCustomer = (req, res) => {
+    const id = req.params.id;
+    Customer.findByIdAndDelete(id)
+        .then(customer=> {
+            if(customer)
+            {
+                console.log("Customer deleted " + id)
+                res.send("Deleted card" + id)
+            }else
+                throw new Error("Could not deleted customer " + id)
+        })
+        .catch(err => {
+            console.log(err.message)
+            res.status(500).json({error: err.message})
+        })
+}   
