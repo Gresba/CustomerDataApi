f9d7c7da0aab130333a8283bb72a88c7f104da04
gresba <kimpaul0709@gmail.com>
2022-07-19 14:37:42 -0400

    Added main root

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import customerRoutes from "./customers.js";
 
 const router = Router();
 
+router.get("/", (req, res) => res.send("Root!"))
 // Default page
 router.get("/api", (req, res) => res.send("API ROOT"))
 
